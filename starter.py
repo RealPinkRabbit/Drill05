bbd16709d38be3349bb7ded5bc512c0ef764c131
RealPinkRabbit <sdg0896@gmail.com>
2023-09-30 21:24:28 +0900

    임의의 위치에 화살표 그리기

--- starter.py
+++ starter.py
@@ -1,5 +1,6 @@
 # 모듈 인포트
 from pico2d import *
+import random
 
 # 캔버스 오픈
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -8,8 +9,9 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 # 리소스 파일 불러오기
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
 
-
+# 키보드 입력 감지 함수
 def handle_events():
     global running
     global x, y
@@ -21,6 +23,8 @@ def handle_events():
             running = False
     pass
 
+def create_hand():
+    hand_arrow.draw(random.randint(0,1280+1), random.randint(0, 1024+1))
 
 # 전역변수 선언
 running = True
@@ -31,12 +35,15 @@ hide_cursor()
 # 메인함수
 while running:
     clear_canvas()
+
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    create_hand()
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
+    delay(0.05)
 
 # 캔버스 닫기
 close_canvas()
