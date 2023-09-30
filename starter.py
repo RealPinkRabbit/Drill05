f7155f6cd896213ccd7e10089b6c1852b77983ca
RealPinkRabbit <sdg0896@gmail.com>
2023-09-30 21:10:49 +0900

    주석추가 + 마우스 따라가기 삭제

--- starter.py
+++ starter.py
@@ -1,8 +1,11 @@
+# 모듈 인포트
 from pico2d import *
 
+# 캔버스 오픈
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
+# 리소스 파일 불러오기
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
@@ -14,19 +17,18 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
 
-
+# 전역변수 선언
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 
+# 메인함수
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -36,6 +38,7 @@ while running:
 
     handle_events()
 
+# 캔버스 닫기
 close_canvas()
 
 
